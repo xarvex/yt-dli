ebae74823bfd4ebd6dbc87a39415f4f9472ecb46
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-09-07 21:28:54 -0500

    feat(interaction): use confirmation dialog for lonely profiles

--- src/interaction.rs
+++ src/interaction.rs
@@ -1,4 +1,6 @@
-use dialoguer::{MultiSelect, theme::ColorfulTheme};
+use std::sync::LazyLock;
+
+use dialoguer::{Confirm, MultiSelect, theme::ColorfulTheme};
 
 use crate::{
     error::{Error, Result},
@@ -6,31 +8,48 @@ use crate::{
     util::sort_dir_results,
 };
 
+static DIALOGUER_THEME: LazyLock<ColorfulTheme> = LazyLock::new(ColorfulTheme::default);
+
 pub fn prompt_profiles() -> Result<Vec<String>> {
     let available_profiles = sort_dir_results(profiles()?)?;
     if available_profiles.is_empty() {
         return Err(Error::MissingProfiles);
     }
 
-    let profile_indices = MultiSelect::with_theme(&ColorfulTheme::default())
-        .with_prompt("Select the profiles to use")
-        .items(&available_profiles)
-        .interact_opt()?
-        .ok_or(Error::Simple("profile selection cancelled"))?;
+    if available_profiles.len() == 1 {
+        let confirm = Confirm::with_theme(&*DIALOGUER_THEME)
+            .with_prompt(format!(
+                "Found '{}' as the only profile. Use it?",
+                available_profiles[0]
+            ))
+            .interact()?;
 
-    if profile_indices.is_empty() {
-        Err(Error::Simple("no profiles were selected"))
+        if confirm {
+            Ok(available_profiles)
+        } else {
+            Err(Error::Simple("profile was rejected"))
+        }
     } else {
-        // TODO: Find a way to avoid cloning and collecting.
-        Ok(profile_indices
-            .iter()
-            .map(|i| available_profiles[*i].clone())
-            .collect())
+        let profile_indices = MultiSelect::with_theme(&*DIALOGUER_THEME)
+            .with_prompt("Select the profiles to use")
+            .items(&available_profiles)
+            .interact_opt()?
+            .ok_or(Error::Simple("profile selection cancelled"))?;
+
+        if profile_indices.is_empty() {
+            Err(Error::Simple("no profiles were selected"))
+        } else {
+            // TODO: Find a way to avoid cloning and collecting.
+            Ok(profile_indices
+                .iter()
+                .map(|i| available_profiles[*i].clone())
+                .collect())
+        }
     }
 }
 
 pub fn prompt_extra_args() -> Result<Vec<String>> {
-    let raw_extra_args = dialoguer::Input::<String>::with_theme(&ColorfulTheme::default())
+    let raw_extra_args = dialoguer::Input::<String>::with_theme(&*DIALOGUER_THEME)
         .with_prompt("Enter arguments to pass to 'yt-dlp'")
         .interact_text()?;
     let extra_args = shlex::split(&raw_extra_args).ok_or(Error::Simple(
